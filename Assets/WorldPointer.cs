0f75a90eaf526affd57408d75a03049dade82997
Mikołaj Dereszewski <mikolaj.dereszewski@gmail.com>
2017-07-04 15:45:58 +0200

    #2: Created basic materials for the scene and written WorldPointer.cs - 3D pointer for Cardboard VR

---
+++ Assets/WorldPointer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPointer : MonoBehaviour {
+
+    private Camera _cameraMain;
+    private Vector3 _cameraPosition;
+    private Renderer _renderer;
+
+	void Start ()
+    {
+        _cameraMain = Camera.main;
+        _cameraPosition = _cameraMain.transform.position;
+        _renderer = GetComponent<Renderer>();
+    }
+	
+	void Update ()
+    {
+        RaycastHit hitInfo;
+		if(Physics.Raycast(_cameraPosition, _cameraMain.transform.forward, out hitInfo))
+        {
+            transform.rotation = Quaternion.LookRotation(hitInfo.normal);
+            transform.position = hitInfo.point + hitInfo.normal * 0.01f;
+            _renderer.enabled = true;
+        }
+        else
+        {
+            _renderer.enabled = false;
+        }
+	}
+}
