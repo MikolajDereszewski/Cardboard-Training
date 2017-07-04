f3004e92e74eccc77eec18b7ef901525396f1df2
Mikołaj Dereszewski <mikolaj.dereszewski@gmail.com>
2017-07-04 15:47:53 +0200

    #3: Created script to shoot the ball after click

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void ShootTheBall()
+    {
+        _rigidbody.AddForce(Camera.main.transform.forward * 2000f);
+    }
+}
